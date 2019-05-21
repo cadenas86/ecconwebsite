8a377519a5029428fd39d1947b6febf6a1be2fa3
Rogelio Gomez <gomezrogelio86@gmail.com>
2019-05-20 21:56:33 -0700

    website

--- assets/js/main.js
+++ assets/js/main.js
@@ -60,3 +60,10 @@ function myFunction() {
 	  x.className = "topnav";
 	}
   }
+
+  // Two images side by side
+function two() {
+	for (i = 0; i < elements.length; i++) {
+	  elements[i].style.flex = "50%"; 
+	}
+  }
